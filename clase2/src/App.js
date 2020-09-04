02560b89ce8aadefed1c504a080c8ff6af5be0b6
Afangar <datruji5@gmail.com>
2020-09-03 23:02:30 -0500

    barrita

--- clase2/src/App.js
+++ clase2/src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import Nav from "./Componentes/nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Clase 2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          jhon se la come
-        </a>
-      </header>
+    <div>
+      <Nav />
     </div>
   );
 }
