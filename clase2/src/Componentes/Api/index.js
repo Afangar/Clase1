a29c3a6da3ffa6521016bf2267ab0b164cd0ebee
Afangar <datruji5@gmail.com>
2020-09-16 22:56:59 -0500

    clases en react

--- clase2/src/Componentes/Api/index.js
+++ clase2/src/Componentes/Api/index.js
@@ -1,58 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../Button/lista.scss";
 import "../Api/Api.scss";
 
-const Api = () => {
-  const [pokemons, setPokemons] = useState({});
+class Api extends Component {
+  state = {
+    pokemons: {},
+  };
 
-  const getPokemons = async () => {
+  componentDidMount() {
+    this.getPokemons();
+  }
+
+  getPokemons = async () => {
     const request1 = await fetch("https://pokeapi.co/api/v2/");
     const response1 = await request1.json();
 
     const request2 = await fetch(response1.pokemon);
     const response = await request2.json();
 
-    setPokemons(response);
+    this.setState({ pokemons: response });
   };
 
-  useEffect(() => {
-    getPokemons();
-  }, []);
-
-  const masPokemons = async () => {
-    const request1 = await fetch(pokemons.next);
+  masPokemons = async () => {
+    const request1 = await fetch(this.state.pokemons.next);
     const response = await request1.json();
 
-    setPokemons(response);
+    this.setState({ pokemons: response });
   };
 
-  const menosPokemons = async () => {
-    const request1 = await fetch(pokemons.previous);
+  menosPokemons = async () => {
+    const request1 = await fetch(this.state.pokemons.previous);
     const response = await request1.json();
 
-    setPokemons(response);
+    this.setState({ pokemons: response });
   };
 
-  return (
-    <div className="api">
-      <h1 className="titulo"> Api Result</h1>
-      <ul className="ul">
-        {pokemons.results
-          ? pokemons.results.map((item) => (
-              <li className="lista">{item.name}</li>
-            ))
-          : ""}
-      </ul>
-      <div>
-        {pokemons.previous ? (
-          <a onClick={menosPokemons}>Pagina anterior</a>
-        ) : (
-          ""
-        )}
-        {pokemons.next ? <a onClick={masPokemons}>Siguiente pagina</a> : ""}
+  render() {
+    return (
+      <div className="api">
+        <h1 className="titulo"> Api Result</h1>
+        <ul className="ul">
+          {this.state.pokemons.results
+            ? this.state.pokemons.results.map((item) => (
+                <li className="lista">{item.name}</li>
+              ))
+            : ""}
+        </ul>
+        <div>
+          {this.state.pokemons.previous ? (
+            <a onClick={this.menosPokemons}>Pagina anterior</a>
+          ) : (
+            ""
+          )}
+          {this.state.pokemons.next ? (
+            <a onClick={this.masPokemons}>Siguiente pagina</a>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Api;
