81d3b729d55bb605201f461e4458517c27b5c33c
Afangar <datruji5@gmail.com>
2020-09-16 22:22:58 -0500

    otrofetch

--- clase2/src/Componentes/Api/index.js
+++ clase2/src/Componentes/Api/index.js
@@ -3,27 +3,39 @@ import "../Button/lista.scss";
 import "../Api/Api.scss";
 
 const Api = () => {
-  const [pokedex, setPokedex] = useState({});
   const [pokemons, setPokemons] = useState({});
 
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/")
-      .then((pokedex) => pokedex.json())
-      .then((obj) => setPokedex(obj));
-  }, []);
+  const getPokemons = async () => {
+    const request1 = await fetch("https://pokeapi.co/api/v2/");
+    const response1 = await request1.json();
+
+    const request2 = await fetch(response1.pokemon);
+    const response = await request2.json();
+
+    setPokemons(response);
+  };
 
   useEffect(() => {
-    if (pokedex.pokemon) {
-      fetch(pokedex.pokemon)
-        .then((response) => response.json())
-        .then((result) => setPokemons(result));
-    }
-  }, [pokedex]);
+    getPokemons();
+  }, []);
+
+  const masPokemons = async () => {
+    const request1 = await fetch(pokemons.next);
+    const response = await request1.json();
+
+    setPokemons(response);
+  };
+
+  const menosPokemons = async () => {
+    const request1 = await fetch(pokemons.previous);
+    const response = await request1.json();
+
+    setPokemons(response);
+  };
 
   return (
     <div className="api">
       <h1 className="titulo"> Api Result</h1>
-      <p className="link">{pokedex.pokedex ? pokedex.pokedex : "aun no"}</p>
       <ul className="ul">
         {pokemons.results
           ? pokemons.results.map((item) => (
@@ -31,6 +43,14 @@ const Api = () => {
             ))
           : ""}
       </ul>
+      <div>
+        {pokemons.previous ? (
+          <a onClick={menosPokemons}>Pagina anterior</a>
+        ) : (
+          ""
+        )}
+        {pokemons.next ? <a onClick={masPokemons}>Siguiente pagina</a> : ""}
+      </div>
     </div>
   );
 };
